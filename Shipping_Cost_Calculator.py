d2e8683a8a5b61fdb5f1228e543814b58bf903a1
alinatitulenko <alinatitulenko@gmail.com>
2026-06-11 13:34:30 +0300

    Revert "revert"
    This reverts commit 8734e3343baa8a38c2a1dfe856744f56b80041fa.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,10 +11,6 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by <alinatitulenko>
  # Here is another update by <alinatitulenko>
- git revert HEAD --no-edit
-
-
-
  
 
 
