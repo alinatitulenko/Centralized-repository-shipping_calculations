8734e3343baa8a38c2a1dfe856744f56b80041fa
alinatitulenko <alinatitulenko@gmail.com>
2026-06-11 13:32:07 +0300

    revert

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,6 +11,10 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by <alinatitulenko>
  # Here is another update by <alinatitulenko>
+ git revert HEAD --no-edit
+
+
+
  
 
 
